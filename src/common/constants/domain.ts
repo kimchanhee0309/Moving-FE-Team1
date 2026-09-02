dad8e761c49ea0e5a868c6fe6084d52a0f9f42bb
apple <apple@appleui-MacBookAir.local>
2026-09-02 17:17:53 +0900

    feat: domain.ts 및 routes.ts 작업

--- src/common/constants/domain.ts
+++ src/common/constants/domain.ts
@@ -1,1 +1,22 @@
-export const USER_ROLE = {};
+export const USER_ROLE = {
+  CUSTOMER: "CUSTOMER",
+  MOVER: "MOVER",
+} as const;
+
+export const SERVICE_TYPE = {
+  SMALL: "SMALL",
+  HOME: "HOME",
+  OFFICE: "OFFICE",
+} as const;
+
+export const QUOTE_STATUS = {
+  PENDING: "PENDING",
+  CONFIRMED: "CONFIRMED",
+  REJECTED: "REJECTED",
+} as const;
+
+export const MOVE_REQUEST_STATUS = {
+  WAITING: "WAITING",
+  CONFIRMED: "CONFIRMED",
+  COMPLETED: "COMPLETED",
+} as const;
