f2151fa6b734f9f760adbd33bb432ba014dfdda8
cari <aa34500388@gmail.com>
2026-09-04 15:07:12 +0900

    feat: Card-list 견적내역 컴포넌트

--- src/features/customer-quote/components/index.ts
+++ src/features/customer-quote/components/index.ts
@@ -1,2 +1,4 @@
 export { QuoteCard } from "./QuoteCard";
 export type { QuoteCardProps } from "./QuoteCard";
+export { QuoteHistoryCard } from "./QuoteHistoryCard";
+export type { QuoteHistoryCardProps } from "./QuoteHistoryCard";
