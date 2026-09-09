f67e3ae901909d73f0bad447d487c03f0ee0e5ee
cari <aa34500388@gmail.com>
2026-09-09 17:53:07 +0900

    fix: 원래상태

--- src/app/(customer)/layout.tsx
+++ src/app/(customer)/layout.tsx
@@ -7,8 +7,7 @@ export default function CustomerLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <GnbContainer />
-      {/* <AuthGuard role="CUSTOMER">{children}</AuthGuard>       */}
-      {children}
+      <AuthGuard role="CUSTOMER">{children}</AuthGuard>      
     </>
   );
 }
