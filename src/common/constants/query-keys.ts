4935ec91764ed5da00006f8e2018b11baeac749d
apple <apple@appleui-MacBookAir.local>
2026-09-16 15:44:50 +0900

    feat: 공통으로 쓰이는 코드 작업

---
+++ src/common/constants/query-keys.ts
@@ -0,0 +1,5 @@
+export const QUERY_KEY_ROOTS = {
+  MOVER_RECEIVED_REQUESTS: ["mover", "received-requests"] as const,
+  MOVER_QUOTES: ["mover", "quotes"] as const,
+  MOVER_REJECTED_REQUESTS: ["mover", "rejected-requests"] as const,
+} as const;
